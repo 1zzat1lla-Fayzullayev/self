e577e193a961c8dd64a585172cbc7362bc4a3a27
Izzatilla <112051978+1zzat1lla-Fayzullayev@users.noreply.github.com>
2024-05-29 00:32:41 +0500

    change

--- src/components/Security.jsx
+++ src/components/Security.jsx
@@ -12,7 +12,7 @@ import code from '../assets/code.png'
 function Tooltip({ text }) {
 	return (
 		<div
-			className='absolute bottom-full left-1/2 transform -translate-x-1/2 mb-2 w-[350px] bg-black text-white text-sm rounded-lg shadow-lg p-[20px] opacity-0 group-hover:opacity-100 transition-opacity duration-300 z-10'
+			className='absolute bottom-full left-1/2 transform mb-2 w-[350px] bg-black text-white text-sm rounded-lg shadow-lg p-[20px] opacity-0 group-hover:opacity-100 transition-opacity duration-300 z-[999]'
 			dangerouslySetInnerHTML={{ __html: text }}
 		/>
 	)
@@ -29,48 +29,47 @@ function Security() {
 						alt='Check'
 						className='w-[230px] md:w-[390px] absolute left-[20%] md:left-[8%] top-[2%] md:top-0'
 					/>
-					<ul className='relative z-20 flex'>
-						<div>
-							<li className='relative group flex items-center text-white font-semibold text-[18px] gap-3'>
-								<img src={code} alt='code' />
+					<ul className='relative z-20 flex flex-col md:flex-row'>
+						<div className='flex flex-col gap-[10px]'>
+							<li className='relative group flex items-center text-white font-semibold text-[18px] gap-3 z-30 cursor-pointer'>
+								<img src={code} alt='code' className=' z-[-1]' />
 								Финансы и банкинг
 								<Tooltip
 									text={`Верификация личности клиентов для борьбы с мошенничеством и отмыванием денег.<br /><br />Безопасная аутентификация для онлайн-банкинга и финансовых сервисов.<br /><br />Управление цифровыми активами и инвестициями с помощью DID.`}
 								/>
 							</li>
-							<li className='relative group flex items-center text-white font-semibold text-[18px] gap-3'>
-								<img src={code} alt='code' />
+							<li className='relative group flex items-center text-white font-semibold text-[18px] gap-3 z-30 cursor-pointer'>
+								<img src={code} alt='code' className=' z-[-1]' />
 								Образование и HR
 								<Tooltip
 									text={`Выдача верифицируемых дипломов, сертификатов и других учетных данных.<br /><br />Подтверждение квалификации и опыта соискателей при найме на работу.<br /><br />Создание децентрализованных систем репутации и портфолио.`}
 								/>
 							</li>
-							<li className='relative group flex items-center text-white font-semibold text-[18px] gap-3'>
-								<img src={code} alt='code' />
+							<li className='relative group flex items-center text-white font-semibold text-[18px] gap-3 z-30 cursor-pointer'>
+								<img src={code} alt='code' className=' z-[-1]' />
 								Электронное правительство
 								<Tooltip
 									text={`Безопасная аутентификация граждан для доступа к госуслугам.<br /><br />Проведение выборов и голосований с верификацией личности избирателей.<br /><br />Борьба с коррупцией через прозрачные реестры на блокчейне.`}
 								/>
 							</li>
 						</div>
-
-						<div>
-							<li className='relative group flex items-center text-white font-semibold text-[18px] gap-3'>
-								<img src={code} alt='code' />
+						<div className='flex flex-col gap-[10px]'>
+							<li className='relative group flex items-center text-white font-semibold text-[18px] gap-3 z-30 cursor-pointer'>
+								<img src={code} alt='code' className=' z-[-1]' />
 								Цепочки поставок и логистика
 								<Tooltip
 									text={`Отслеживание происхождения и подлинности товаров.<br /><br />Верификация личности участников цепочки поставок.<br /><br />Автоматизация таможенного оформления и сертификации грузов.`}
 								/>
 							</li>
-							<li className='relative group flex items-center text-white font-semibold text-[18px] gap-3'>
-								<img src={code} alt='code' />
+							<li className='relative group flex items-center text-white font-semibold text-[18px] gap-3 z-30 cursor-pointer'>
+								<img src={code} alt='code' className=' z-[-1]' />
 								Недвижимость и земельные кадастры
 								<Tooltip
 									text={`Создание надежных реестров прав собственности на базе блокчейна <br /><br />Упрощение процедур купли-продажи и регистрации недвижимости <br /><br />Токенизация недвижимости для инвестиций и коллективного владения`}
 								/>
 							</li>
-							<li className='relative group flex items-center text-white font-semibold text-[18px] gap-3'>
-								<img src={code} alt='code' />
+							<li className='relative group flex items-center text-white font-semibold text-[18px] gap-3 z-30 cursor-pointer'>
+								<img src={code} alt='code' className=' z-[-1]' />
 								Страхование
 								<Tooltip
 									text={`Верификация личности страхователей для борьбы с мошенничеством <br /><br />Безопасный обмен данными с андеррайтерами и медучреждениями <br /><br />Автоматизированная оценка рисков и персонализация страховых продуктов`}
