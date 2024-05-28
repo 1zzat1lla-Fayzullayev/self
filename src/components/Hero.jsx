a55d319ded7fe86a4c0db05d06c72a047d40eb36
Izzatilla <112051978+1zzat1lla-Fayzullayev@users.noreply.github.com>
2024-05-28 22:52:37 +0500

    change

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -3,8 +3,8 @@ import React from 'react'
 function Hero() {
 	return (
 		<>
-			<div className='flex justify-center items-center pt-[100px]'>
-				<video
+			<div className='flex justify-center items-center pt-[250px] md:pt-[300px] pb-[200px] md:pb-[100px]'>
+				{/* <video
 					src='https://safe.global/videos/Home/safe-2024-hero-vp9.webm'
 					autoPlay
 					playsInline
@@ -20,10 +20,15 @@ function Hero() {
 						src='https://safe.global/videos/Home/safe-2024-hero.mp4'
 						type='video/mp4'
 					/>
-				</video>
-				<h1 className='absolute text-[50px] md:text-[70px] text-white w-full md:w-[50%] font-semibold text-center leading-[55px] md:leading-[70px]'>
-					Smart Accounts to{' '}
-					<span className='text-[#12FF80]'>Own the Internet</span>{' '}
+				</video> */}
+				<h1 className='absolute text-[35px] md:text-[60px] text-white w-full md:w-[50%] font-semibold text-center leading-[55px] md:leading-[70px] '>
+					Децентрализованная идентификация для{' '}
+					<span
+						className='text-[rgb(0 170 255)]'
+						style={{ color: 'rgb(0 170 255)' }}
+					>
+						защиты ваших данных
+					</span>{' '}
 				</h1>
 			</div>
 		</>
