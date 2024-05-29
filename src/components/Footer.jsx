a67775c7bae0fedcc9454152e6be63dc5523fcb8
Izzatilla <112051978+1zzat1lla-Fayzullayev@users.noreply.github.com>
2024-05-30 04:56:07 +0500

    change

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -7,13 +7,12 @@ function Footer() {
 		<>
 			<Wrapper>
 				<div className='flex flex-col md:flex-row md:items-start items-center md:justify-between justify-center gap-[10px] pt-[100px] h-full pb-[40px]'>
-					<img src={logo} alt='logo' className='cursor-pointer w-[50px] md:w-[70px]' />
+					<img
+						src={logo}
+						alt='logo'
+						className='cursor-pointer w-[70px] md:w-[90px]'
+					/>
 					<ul className='flex flex-col items-center md:items-start gap-3 font-semibold'>
-						<li>
-							<a href='#' className='uppercase text-white font-semibold'>
-								developers
-							</a>
-						</li>
 						{/* <li>
 							<a href='#' className='text-[#848689]'>
 								Indao (Core)
@@ -26,11 +25,11 @@ function Footer() {
 						</li> */}
 					</ul>
 					<ul className='flex flex-col items-center md:items-start gap-3 font-semibold'>
-						<li>
+						{/* <li>
 							<a href='#' className='uppercase text-white font-semibold'>
 								ecosystem
 							</a>
-						</li>
+						</li> */}
 						{/* <li>
 							<a href='#' className='text-[#848689]'>
 								Explore
@@ -48,26 +47,24 @@ function Footer() {
 								community
 							</a>
 						</li>
-						{/* <li>
-							<a href='#' className='text-[#848689]'>
-								Governance
+						<li>
+							<a
+								href='https://x.com/inDAO_io'
+								target='_blank'
+								className='text-[#848689]'
+							>
+								X (Twitter)
 							</a>
 						</li>
 						<li>
-							<a href='#' className='text-[#848689]'>
-								Token
+							<a
+								href='https://snapshot.org/#/indaonetwork.eth'
+								target='_blank'
+								className='text-[#848689]'
+							>
+								Snapshot
 							</a>
 						</li>
-						<li>
-							<a href='#' className='text-[#848689]'>
-								Indao(Pass)
-							</a>
-						</li>
-						<li>
-							<a href='#' className='text-[#848689]'>
-								Indao(Con)
-							</a>
-						</li> */}
 					</ul>
 					<ul className='flex flex-col items-center md:items-start gap-3 font-semibold'>
 						<li>
@@ -75,26 +72,15 @@ function Footer() {
 								resources
 							</a>
 						</li>
-						{/* <li>
-							<a href='#' className='text-[#848689]'>
-								Help Center
+						<li>
+							<a
+								href='https://indao.gitbook.io/did-technology'
+								target='_blank'
+								className='text-[#848689]'
+							>
+								Gitbook
 							</a>
 						</li>
-						<li>
-							<a href='#' className='text-[#848689]'>
-								Press Room
-							</a>
-						</li>
-						<li>
-							<a href='#' className='text-[#848689]'>
-								Careers
-							</a>
-						</li>
-						<li>
-							<a href='#' className='text-[#848689]'>
-								Brand Kit
-							</a>
-						</li> */}
 					</ul>
 				</div>
 			</Wrapper>
