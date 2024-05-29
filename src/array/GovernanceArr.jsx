2b671dec9d955ac6e99428f73b7dc06e92d67bf0
Izzatilla <112051978+1zzat1lla-Fayzullayev@users.noreply.github.com>
2024-05-30 03:50:39 +0500

    change

--- src/array/GovernanceArr.jsx
+++ src/array/GovernanceArr.jsx
@@ -2,14 +2,17 @@ const governance = [
 	{
 		title: 'be part of a community',
 		desc: 'INDAO Token',
+		href: 'https://x.com/inDAO_io',
 	},
 	{
 		title: 'join a discussion',
 		desc: 'Latest Proposals',
+		href: 'https://snapshot.org/#/indaonetwork.eth',
 	},
 	{
 		title: 'learn more',
 		desc: 'Governance Hub',
+		href: 'https://app.gitbook.com/o/2iADhsvChdaBjLq3rqrX',
 	},
 ]
 
--- src/components/Governance.jsx
+++ src/components/Governance.jsx
@@ -27,38 +27,44 @@ function Governance() {
 					</div>
 					<div className='flex flex-col md:flex-row justify-center items-center gap-[15px] mt-[70px] mx-[20px] md:mx-0'>
 						{governance.map(card => (
-							<div
-								key={card.title}
+							<a
+								href={card.href}
+								target='_blank'
 								className='relative card1 flex flex-col border-[1px] rounded-[20px] border-[#303033] hover:border-[#12FF80] max-w-[400px] h-[220px] cursor-pointer w-full shadow-lg bg-[#121312]'
 							>
-								<p className='text-[#A1A3A7] font-semibold mt-[30px] ml-[15px] uppercase'>
-									{card.title}
-								</p>
-								{/* Inline SVG */}
-								<div className='w-[30px] arrow absolute right-5 top-4 transform transition-all ease-in rotate-[320deg]'>
-									<svg
-										width='35'
-										height='35'
-										viewBox='0 0 24 24'
-										fill='none'
-										xmlns='http://www.w3.org/2000/svg'
-										className='text-white arrow'
-									>
-										<path
-											d='M5 12h14M12 5l7 7-7 7'
-											stroke='currentColor'
-											strokeWidth='1.5'
-											strokeLinecap='round'
-											strokeLinejoin='round'
-										/>
-									</svg>
+								<div
+									key={card.title}
+									className='relative card1 flex flex-col border-[1px] rounded-[20px] border-[#303033] hover:border-[#12FF80] max-w-[400px] h-[220px] cursor-pointer w-full shadow-lg bg-[#121312]'
+								>
+									<p className='text-[#A1A3A7] font-semibold mt-[30px] ml-[15px] uppercase'>
+										{card.title}
+									</p>
+									{/* Inline SVG */}
+									<div className='w-[30px] arrow absolute right-5 top-4 transform transition-all ease-in rotate-[320deg]'>
+										<svg
+											width='35'
+											height='35'
+											viewBox='0 0 24 24'
+											fill='none'
+											xmlns='http://www.w3.org/2000/svg'
+											className='text-white arrow'
+										>
+											<path
+												d='M5 12h14M12 5l7 7-7 7'
+												stroke='currentColor'
+												strokeWidth='1.5'
+												strokeLinecap='round'
+												strokeLinejoin='round'
+											/>
+										</svg>
+									</div>
+									<div className='mt-[80px]'>
+										<h2 className='font-semibold text-[30px] ml-[15px] text-white'>
+											{card.desc}
+										</h2>
+									</div>
 								</div>
-								<div className='mt-[80px]'>
-									<h2 className='font-semibold text-[30px] ml-[15px] text-white'>
-										{card.desc}
-									</h2>
-								</div>
-							</div>
+							</a>
 						))}
 					</div>
 					<div className='flex justify-center items-center flex-col mt-[70px] gap-[40px]'>
