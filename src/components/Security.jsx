add434cc0f8eda6fb09fb740bb5044f2a4e5d928
Izzatilla <112051978+1zzat1lla-Fayzullayev@users.noreply.github.com>
2024-05-29 19:53:43 +0500

    change

--- src/components/Security.jsx
+++ src/components/Security.jsx
@@ -15,11 +15,15 @@ function Security() {
 		<Wrapper>
 			<div className='relative flex justify-center flex-col md:flex-row items-start mt-[150px]'>
 				<div className='flex flex-col mx-[20px] md:mx-0'>
-					<img src={checkbg} alt='Background' className='z-10 w-full md:max-w-[700px]' />
+					<img
+						src={checkbg}
+						alt='Background'
+						className='z-10 w-full md:max-w-[700px]'
+					/>
 					<img
 						src={check}
 						alt='Check'
-						className='w-[230px] md:w-[390px] absolute left-[20%] md:left-[14%] top-[2%] md:top-[10%] z-20'
+						className='w-[230px] md:w-[390px] absolute left-[20%] md:left-[14%] top-[2%] md:top-[14%] z-20'
 					/>
 					<ul className='relative z-30 flex flex-col md:flex-row mt-[100px] mb-[50px]'>
 						<div className='flex flex-col gap-[10px] w-full'>
@@ -44,7 +48,9 @@ function Security() {
 								<Tooltip
 									text={`Безопасная аутентификация граждан для доступа к госуслугам. Проведение выборов и голосований с верификацией личности избирателей. Борьба с коррупцией через прозрачные реестры на блокчейне.`}
 								>
-									<p className='w-full md:w-[350px]'>Электронное правительство</p>
+									<p className='w-full md:w-[350px]'>
+										Электронное правительство
+									</p>
 								</Tooltip>
 							</li>
 						</div>
@@ -54,7 +60,9 @@ function Security() {
 								<Tooltip
 									text={`Отслеживание происхождения и подлинности товаров. Верификация личности участников цепочки поставок. Автоматизация таможенного оформления и сертификации грузов.`}
 								>
-									<p className='w-full md:w-[350px]'>Цепочки поставок и логистика</p>
+									<p className='w-full md:w-[350px]'>
+										Цепочки поставок и логистика
+									</p>
 								</Tooltip>
 							</li>
 							<li className='group flex items-center text-white font-semibold text-[18px] gap-3 cursor-pointer'>
@@ -62,7 +70,9 @@ function Security() {
 								<Tooltip
 									text={`Создание надежных реестров прав собственности на базе блокчейна. Упрощение процедур купли-продажи и регистрации недвижимости. Токенизация недвижимости для инвестиций и коллективного владения.`}
 								>
-									<p className='w-full md:w-[350px]'>Недвижимость и земельные кадастры</p>
+									<p className='w-full md:w-[350px]'>
+										Недвижимость и земельные кадастры
+									</p>
 								</Tooltip>
 							</li>
 							<li className='group flex items-center text-white font-semibold text-[18px] gap-3 cursor-pointer'>
@@ -78,14 +88,13 @@ function Security() {
 				</div>
 				<div className='mx-[20px] md:mx-0 md:w-[50%]'>
 					<p className='text-[#A1A3A7] font-semibold'>SECURITY</p>
-					<h2 className='text-[40px] md:text-[60px] text-white leading-[55px] mt-[30px]'>
-						Indao Smart Accounts are the{' '}
-						<span className='text-[#12FF80]'>most audited</span> and{' '}
-						<span className='text-[#12FF80]'>battle-tested</span>{' '}
-					</h2>
 					<p className='text-white font-semibold text-[20px] mt-[30px]'>
-						Since 2018, our smart contracts have passed the highest possible
-						security standards in the industry including formal verification.
+						inDAO предлагает решения на базе блокчейна и криптографии, такие как
+						децентрализованные идентификаторы (DID) и доказательства с нулевым
+						разглашением (zkproof), для безопасной интеграции
+						блокчейн-технологий в различные отрасли, включая здравоохранение,
+						финансы, государственное управление, цепочки поставок и многие
+						другие.
 					</p>
 					<div className='flex items-center flex-wrap gap-[30px] mt-[40px]'>
 						<img src={security2} alt='' className='w-[100px]' />
